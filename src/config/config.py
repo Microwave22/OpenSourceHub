ba0f7b2bb1ef859cc3e02ed353d061449052e08f
Logan Purcell <loganpurcell20@protonmail.com>
2020-08-10 15:44:25 -0400

    Fixed all Bugs that have been found

--- src/config/config.py
+++ src/config/config.py
@@ -1,46 +1,46 @@
 #!/usr/bin/python3
 import os
 import json
-from pathlib import Path
 
 class Config(object):
 # CRUD App like config manager
   def __init__(self, config_path):
     self.config_path = config_path
     self.config_file = self.loadConfig()
-
+    
   def loadConfig(self):
     try:
-      with open(self.config_path) as f:
+      with open(str(self.config_path), "r+") as f:
         config_file = json.loads(f.read())
-      return config_file
+        return config_file
     except FileNotFoundError:
-      print("File Does Not Exist, Assuming you will be using GenerateConfig")
-   
+      print("File Does Not Exist, Assuming you will be using Generate Config")
+
   def generateConfig(self, ConnectionServer="localhost", ConnectionPort=5000, AllowOnionIp=False):
-    Path(self.config_path).touch()
+    os.mknod(str(self.config_path))
     configSettings = {
       'connection_server': f'{ConnectionServer}',
       'connection_port': f'{ConnectionPort}',
       'allow_onion_ip': f'{AllowOnionIp}'
     }
-    with open(self.config_path) as fp:
+    with open(self.config_path, "w") as fp:
       configJson = json.dump(configSettings, fp)
       fp.close()
+    self.config_file = self.loadConfig()
+
 
   def readVariable(self, config_variable):
-    config_json = json.loads(self.config_file)
-    return config_json[config_variable]
+    return print(self.config_file[config_variable])
 
   def changeVariable(self, config_variable, new_value):
-    config_json = json.loads(self.config_file)
-    config_json[config_variable] = new_value
-    json.dumps(config_json, self.config_path)
-    return config_json
+    self.config_file[str(config_variable)] = str(new_value)
+    config_path = open(self.config_path, 'r+')
+    json.dump(self.config_file, config_path)
+    return self.config_file
 
   def deleteVariable(self, config_variable):
-    config_json = json.loads(self.config_file)
-    config_json.pop(config_variable)
-    json.dumps(config_json, self.config_path)
-    return config_json
+    self.config_file.pop(config_variable)
+    config_path = open(self.config_path, 'w')
+    json.dump(self.config_file, config_path)
+    return self.config_file
 
